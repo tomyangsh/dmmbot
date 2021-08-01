ae48a5f1f7415f8b25b5f74fc2919eaa23babda5
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2021-08-01 18:54:06 -0400

    use javlibrary to get cid

--- dmmbot.py
+++ dmmbot.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # coding: utf-8
 
-import tempfile, os, requests, urllib.request, shutil, asyncio, re, random
+import os, requests, urllib.request, shutil, asyncio, re, random, tempfile
+
+from io import BytesIO
 
 from telethon import TelegramClient, events
 from telethon.tl.types import DocumentAttributeFilename, DocumentAttributeVideo, DocumentAttributeImageSize
@@ -13,10 +15,11 @@ from hachoir.metadata.video import MP4Metadata
 
 from FastTelethon.FastTelethon import upload_file
 
+token = os.getenv("TOKEN")
 app_id = int(os.getenv("APP_ID"))
 app_hash = os.getenv("APP_HASH")
 
-bot = TelegramClient('bot', app_id, app_hash)
+bot = TelegramClient('bot', app_id, app_hash).start(bot_token=token)
 
 def get_metadata(save_path):
     metadata = extractMetadata(createParser(save_path))
@@ -37,43 +40,30 @@ async def send_pic(event):
     chat_id = event.message.chat_id
     label = ['ssis', 'ssni', 'snis', 'soe', 'oned', 'onsd', 'ebod', 'jufe', 'juy', 'jul', 'mide', 'mifd', 'miaa', 'ipx', 'mird', 'dasd', 'pppd', 'dandy', 'dvdms', 'stars', 'sdmu', 'meyd']
     num = random.choice(label)+'-'+f'{random.randrange(1, 999):03}'
-    infopage = requests.post('https://www.jav321.com/search', data={'sn':num})
-    if infopage.url == 'https://www.jav321.com/search':
-        num = random.choice(label)+'-'+f'{random.randrange(1, 999):03}'
-        infopage = requests.post('https://www.jav321.com/search', data={'sn':num})
-    cid = re.sub('https://www.jav321.com/video/', '', infopage.url)
-    picurl = 'https://pics.dmm.co.jp/digital/video/'+cid+'/'+cid+'pl.jpg'
-    temp_dir = tempfile.TemporaryDirectory()
-    save_path = temp_dir.name+'/'+cid+'pl.jpg'
-    with urllib.request.urlopen(picurl) as response, open(save_path, 'wb') as out_file:
-        shutil.copyfileobj(response, out_file)
-    await bot.send_file(chat_id, save_path, caption=num)
-
-    temp_dir.cleanup()
+    infopage = requests.get('http://www.javlibrary.com/cn/vl_searchbyid.php?keyword={}'.format(num)).text
+    cid = re.search('pics.dmm.co.jp/mono/movie/adult/(\w+)/', infopage).groups()[0]
+    picurl = 'https://pics.dmm.co.jp/mono/movie/adult/'+cid+'/'+cid+'pl.jpg'
+    image = BytesIO(requests.get(picurl).content)
+    await bot.send_file(chat_id, image, caption=num)
 
 
 @bot.on(events.NewMessage(pattern=r'/dmmpic\s*.*-\d*'))
 async def send_pic(event):
     chat_id = event.message.chat_id
     num = re.sub(r'/dmmpic\s*', '', event.message.text)
-    infopage = requests.post('https://www.jav321.com/search', data={'sn':num})
-    cid = re.sub('https://www.jav321.com/video/', '', infopage.url)
-    picurl = 'https://pics.dmm.co.jp/digital/video/'+cid+'/'+cid+'pl.jpg'
-    temp_dir = tempfile.TemporaryDirectory()
-    save_path = temp_dir.name+'/'+cid+'pl.jpg'
-    with urllib.request.urlopen(picurl) as response, open(save_path, 'wb') as out_file:
-        shutil.copyfileobj(response, out_file)
-    await bot.send_file(chat_id, save_path)
-
-    temp_dir.cleanup()
+    infopage = requests.get('http://www.javlibrary.com/cn/vl_searchbyid.php?keyword={}'.format(num)).text
+    cid = re.search('pics.dmm.co.jp/mono/movie/adult/(\w+)/', infopage).groups()[0]
+    picurl = 'https://pics.dmm.co.jp/mono/movie/adult/'+cid+'/'+cid+'pl.jpg'
+    image = BytesIO(requests.get(picurl).content)
+    await bot.send_file(chat_id, image)
 
 @bot.on(events.NewMessage(pattern=r'V|v'))
 async def send_vid(event):
     chat_id = event.message.chat_id
     reply_msg = await event.get_reply_message()
     num = re.match(r'.+-\d+', reply_msg.text).group()
-    infopage = requests.post('https://www.jav321.com/search', data={'sn':num})
-    cid = re.sub('https://www.jav321.com/video/', '', infopage.url)
+    infopage = requests.get('http://www.javlibrary.com/cn/vl_searchbyid.php?keyword={}'.format(num)).text
+    cid = re.search('pics.dmm.co.jp/mono/movie/adult/(\w+)/', infopage).groups()[0]
     cidp = cid[0]+'/'+cid[0:3]+'/'+cid
     vidurl = 'https://cc3001.dmm.co.jp/litevideo/freepv/'+cidp+'/'+cid+'_mhb_w.mp4'
     if requests.get(vidurl).status_code == 404:
@@ -98,15 +88,14 @@ async def send_vid(event):
     ]
     input_media.mime_type = mime_type
     await bot.send_file(chat_id, input_media)
-
     temp_dir.cleanup()
 
 @bot.on(events.NewMessage(pattern=r'/dmmvid\s*.+-\d+'))
 async def send_vid(event):
     chat_id = event.message.chat_id
     num = re.sub(r'/dmmvid\s*', '', event.message.text)
-    infopage = requests.post('https://www.jav321.com/search', data={'sn':num})
-    cid = re.sub('https://www.jav321.com/video/', '', infopage.url)
+    infopage = requests.get('http://www.javlibrary.com/cn/vl_searchbyid.php?keyword={}'.format(num)).text
+    cid = re.search('pics.dmm.co.jp/mono/movie/adult/(\w+)/', infopage).groups()[0]
     cidp = cid[0]+'/'+cid[0:3]+'/'+cid
     vidurl = 'https://cc3001.dmm.co.jp/litevideo/freepv/'+cidp+'/'+cid+'_mhb_w.mp4'
     if requests.get(vidurl).status_code == 404:
@@ -131,9 +120,7 @@ async def send_vid(event):
     ]
     input_media.mime_type = mime_type
     await bot.send_file(chat_id, input_media)
-
     temp_dir.cleanup()
-
 
 if __name__ == '__main__':
     bot.start()
